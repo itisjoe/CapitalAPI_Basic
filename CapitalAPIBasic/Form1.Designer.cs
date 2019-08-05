f7939e1c4067d9a81928c01008751fc4dcfbe0db
itisjoe <itisjoe@gmail.com>
2019-08-05 15:27:53 +0800

    initial commit

---
+++ CapitalAPIBasic/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace CapitalAPIBasic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(80, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(250, 39);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(404, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(250, 39);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 57);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox3.Size = new System.Drawing.Size(760, 492);
+            this.textBox3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 31);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "帳號";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(336, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 31);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "密碼";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(672, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 39);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "登入";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Margin = new System.Windows.Forms.Padding(8, 8, 8, 8);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ CapitalAPIBasic/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SKCOMLib;
+
+namespace CapitalAPIBasic
+{
+    public partial class Form1 : Form
+    {
+        SKCenterLib skc;
+        SKReplyLib skr;
+        SKQuoteLib skq;
+
+        private string acc = "";
+        private string pw = "";
+        private int connect_status = 999;
+        private string stockNo = "TX00";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            skc = new SKCenterLib();
+            skr = new SKReplyLib();
+            skr.OnReplyMessage += new _ISKReplyLibEvents_OnReplyMessageEventHandler(OnAnnouncement);
+            skq = new SKQuoteLib();
+            skq.OnConnection += new _ISKQuoteLibEvents_OnConnectionEventHandler(OnConnection);
+            skq.OnNotifyTicks += new _ISKQuoteLibEvents_OnNotifyTicksEventHandler(OnNotifyTicks);
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            acc = textBox1.Text;
+            pw = textBox2.Text;
+            connect_status = skc.SKCenterLib_Login(acc, pw);
+            if (connect_status == 0)
+            {
+                msg_update("登入成功");
+                connect_status = skq.SKQuoteLib_EnterMonitor();
+                if (connect_status == 0)
+                {
+                    msg_update("連線成功");
+                }
+                else
+                {
+                    msg_update("連線錯誤代碼： " + Convert.ToString(connect_status));
+                }
+            }
+            else
+            {
+                msg_update("登入錯誤代碼： " + Convert.ToString(connect_status));
+            }
+        }
+
+        void OnConnection(int nKind, int nCode)
+        {
+            if (nKind == 3001)
+            {
+                if (nCode == 0)
+                {
+                    msg_update("連線中");
+                }
+            }
+            else if (nKind == 3002)
+            {
+                msg_update("斷線");
+            }
+            else if (nKind == 3003)
+            {
+                msg_update("報價商品載入完成");
+                StartRequest();
+            }
+            else if (nKind == 3021)//網路斷線
+            {
+                msg_update("連線失敗");
+            }
+        }
+
+        void StartRequest()
+        {
+            connect_status = skq.SKQuoteLib_RequestTicks(1, stockNo);
+            if (connect_status == 0)
+            {
+                msg_update(stockNo + " 成交明細與五檔設置成功");
+            }
+            else
+            {
+                msg_update("成交明細與五檔錯誤代碼： " + Convert.ToString(connect_status));
+            }
+        }
+
+        void OnNotifyTicks(short sMarketNo, short sStockIdx, int nPtr, int nDate, int lTimehms, int lTimemillismicros, int nBid, int nAsk, int nClose, int nQty, int nSimulate)
+        {
+            string strData = "";
+            strData = sStockIdx.ToString() + "," + nPtr.ToString() + "," + nDate.ToString() + " " + lTimehms.ToString() + "," + nBid.ToString() + "," + nAsk.ToString() + "," + nClose.ToString() + "," + nQty.ToString();
+            msg_update(strData);
+        }
+
+        void OnAnnouncement(string strUserID, string bstrMessage, out short nConfirmCode)
+        {
+            nConfirmCode = -1;
+        }
+
+        private void msg_update(string msg)
+        {
+            textBox3.Text += msg + "\r\n";
+        }
+
+    }
+}
